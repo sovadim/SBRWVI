2239f4e5bb88d9d637b98f990cf83bd6e726a6a3
sovadim <vadik.sofin@yandex.ru>
2019-06-05 22:45:02 +0300

    arduino

---
+++ source/Arduino/system/hands.h
@@ -0,0 +1,41 @@
+#pragma once
+
+// HANDS
+
+void test_hands()
+{
+  
+}
+
+// Shoulders
+void left_shoulder()
+{
+  
+}
+
+void right_shoulder()
+{
+  
+}
+
+// Elbows
+void left_elbow()
+{
+  
+}
+
+void right_elbow()
+{
+  
+}
+
+// Wrists
+void left_wrist()
+{
+  
+}
+
+void right_wrist()
+{
+  
+}
---
+++ source/Arduino/system/system.ino
@@ -0,0 +1,12 @@
+#include "hands.h"
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+
+  test_hands();
+
+}
